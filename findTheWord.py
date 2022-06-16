890abe5e0b388fdf02c184208d35bde6e915c6c6
idilc10 <106824743+idilc10@users.noreply.github.com>
2022-06-16 00:46:14 -0800

    Find The word
    Finds the word that you enter

---
+++ findTheWord.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 16 11:39:03 2022
+
+@author: IFK
+"""
+setListe={"İdil","Fuat","Kaan"}
+word=input("Write a word \n")
+if word in setListe:
+    print("The word is in list")
+else:
+    print("The word is not in list")
