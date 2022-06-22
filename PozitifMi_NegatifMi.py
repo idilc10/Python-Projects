8deb68875daf6bb76cdca7dd73d272c99193168f
D. İdil ÇİMEN KARAKUŞ <106824743+idilc10@users.noreply.github.com>
2022-06-22 06:30:29 -0800

    Add files via upload

---
+++ PozitifMi_NegatifMi.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 22 17:18:21 2022
+
+@author: IFK
+"""
+
+x=float(input("Please enter a number:\n"))
+
+if x<0.0:
+    print("The number is negative")
+elif x>0.0:
+    print("The number is positive")
+elif x==0.0:
+    print("The number is equal to zero")
